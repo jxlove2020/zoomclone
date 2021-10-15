45e28b341f99542cfd9f37b5834e33a98c67f07d
Joseph <jxlove2020@gmail.com>
2021-10-15 16:59:25 +0900

    websocket 설치 및 pug 설치

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-app.use('public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
