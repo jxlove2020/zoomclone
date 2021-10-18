97194e2f0ce09b3380262fd783b642cb92c2fc16
Joseph <jxlove2020@gmail.com>
2021-10-18 16:40:41 +0900

    SocketIO 사용

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
 import express from 'express';
 
 const app = express();
@@ -10,33 +10,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
-
 // http 서버위에 wss 서버를 만듦, 동일한 포트에서 http, wss 사용
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-wss.on('connection', socket => {
-  sockets.push(socket);
-  socket['nickname'] = 'Anonymous';
-  console.log('Connected to Browser ✅');
-  socket.on('close', () => {
-    console.log('Disconnected from the Server ❌');
-  });
-  socket.on('message', msg => {
-    // console.log(msg.toString());
-    const message = JSON.parse(msg.toString());
-    switch (message.type) {
-      case 'new_message':
-        sockets.forEach(aSocket =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case 'nickname':
-        socket['nickname'] = message.payload;
-    }
-  });
+wsServer.on('connection', socket => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
